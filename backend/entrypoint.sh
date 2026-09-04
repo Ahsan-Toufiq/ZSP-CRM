fbb7063ad62985313d17c9534d617631c72bf41d
Ahsan Toufiq <ahsantoufiq@hotmail.com>
2026-09-04 11:40:16 +0500

    Run Render job commands through shell

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -2,10 +2,7 @@
 set -eu
 
 if [ "$#" -gt 0 ]; then
-    if [ "$#" -eq 1 ]; then
-        exec /bin/sh -c "$1"
-    fi
-    exec "$@"
+    exec /bin/sh -c "$*"
 fi
 
 python manage.py migrate --noinput
